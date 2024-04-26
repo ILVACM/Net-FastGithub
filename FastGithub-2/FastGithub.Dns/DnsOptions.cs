d13c090321d22b6b5ac81fa646e9f58da2992f14
ILVACM <3347620766@qq.com>
2024-04-26 20:58:11 +0800

    storage

---
+++ FastGithub-2/FastGithub.Dns/DnsOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace FastGithub.Dns
+{
+    [Options("Dns")]
+    sealed class DnsOptions
+    {
+        public IPAddress UpStream { get; set; } = IPAddress.Parse("114.114.114.114");
+    }
+}
---
+++ FastGithub-2/FastGithub/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace FastGithub
+{
+    class Program
+    {
+        /// <summary>
+        /// 程序入口
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// 创建host
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host
+                .CreateDefaultBuilder(args)
+                .ConfigureServices((ctx, services) =>
+                {
+                    services.AddGithubDns(ctx.Configuration);
+                });
+        }
+    }
+}
